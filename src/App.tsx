5de83fe9e365c04b1d36c1eb1c2ec50cc2ea509b
Lúcio Andrade <lucioandradejr@gmail.com>
2023-09-13 03:07:40 -0400

    feat: prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { Button } from "./components/ui/button";
+import { Button } from './components/ui/button';
 
 export function App() {
   return (
-  <div className="bg-red-500 p-2">
-  <Button>nao</Button>
-  <button>nao</button>
-  </div>
-  )
+    <main>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="font-semibold">upload.ai</h1>
+        <div>
+          <span>Desenvolvido com 💜 na NLW da Rocketseat</span>
+          <Button>
+            <a href="https://github.com/lucioreyli/upload-ai-web-client">
+              GitHub
+            </a>
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
 }
-
